9209ebd23cf6a9140a281a63837315ddeedc0604
whoJahanzeb <i.am.jahanzeb6@gmail.com>
2025-11-24 22:46:38 +0500

    create and design project section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { ArrowRight, ExternalLink, Github } from 'lucide-react';
 const projects = [
     {
         id: 1,
-        title: "MovieGo - Movie Landing Page",
+        title: "Movie Landing Page",
         description: "A responsive movie landing page built with React and TailwindCSS.",
         image: "/projects/project1.png",
         tags: ["React", "TailwindCSS", "Vercel"],
@@ -23,7 +23,7 @@ const projects = [
         id: 3,
         title: "Music Library App",
         description:
-            "A responsive landing page for a music library platform, built using Next.js and React.js with a modern UI and smooth user experience.",
+            "A responsive page for a music library platform, built using Next.js and React.js.",
         image: "/projects/project3.png",
         tags: ["React", "Node.js", "Stripe"],
         demoUrl: "https://music-library-azure.vercel.app/",
@@ -37,8 +37,46 @@ const ProjectsSection = () => {
                 Featured <span className='text-primary'>Projects</span>
             </h2>
             <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
-                A selection of projects showcasing my skills and experience.
+                Here are some of my recent projects. Each project was carefully crafted with attention to detail and a focus on delivering a great user experience.
             </p>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {projects.map((project, key) => (
+                    <div key={key} className='group bg-card rounded-lg overflow-hidden shadow-xs card-hover'>
+                        <div className='h-48 overflow-hidden'>
+                            <img src={project.image} alt={project.title} className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110' />
+                        </div>
+
+                        <div className='p-6'>
+                            <div className='flex flex-wrap gap-2 mb-4'>
+                                {project.tags.map((tag, index) => (
+                                    <span key={index} className='text-xs border bg-secondary text-secondary-foreground px-2 py-1 rounded-full'>
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+
+
+                            <h3 className='text-xl font-semibold mb-1'>{project.title}</h3>
+                            <p className='text-muted-foreground text-sm mb-4'>{project.description}</p>
+
+                            <div className='flex justify-between items-center'>
+                                <div className='flex space-x-3'>
+                                    <a href={project.demoUrl} target='_blank' className='text-foreground/80 hover:text-primary transition-colors duration-300'><ExternalLink size={20} /></a>
+                                    <a href={project.githubUrl} target='_blank' className='text-foreground/80 hover:text-primary transition-colors duration-300'><Github size={20} /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className='text-center mt-12'>
+                <a href="https://github.com/whoJahanzeb" target='_blank' className='cosmic-button w-fit flex items-center mx-auto gap-2'>
+                    Check My GitHub <ArrowRight size={16}/>
+                </a>
+            </div>
+
         </div>
     </section>
 }
