eca9492aec4df9a3e20a8475ee1d93b4f6ea5732
whoJahanzeb <i.am.jahanzeb6@gmail.com>
2025-11-26 21:28:07 +0500

    create footer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import AboutSection from '../components/AboutSection'
 import SkillsSection from '../components/SkillsSection'
 import ProjectsSection from '../components/ProjectsSection'
 import ContactSection from '../components/ContactSection'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -25,6 +26,8 @@ const Home = () => {
         <ProjectsSection />
         <ContactSection />
       </main>
+
+      <Footer/>
     </div>
   )
 }
